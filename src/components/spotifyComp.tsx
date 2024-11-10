7aade7e0d23cd74d31004e6918bb47982b25869c
Ankit <rattanankit2004@gmail.com>
2024-11-10 11:35:43 +0530

    addtwospot

---
+++ src/components/spotifyComp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SpotifyPlaylist = () => (
+  <iframe
+    src="https://open.spotify.com/playlist/3abbn1rg9ZbJfHMk317pCh?si=al_NVi5zTDaKbfjeXU0o3A "
+    width="100%"
+    height="380"
+    frameBorder="0"
+    allow="encrypted-media"
+  ></iframe>
+);
+
+export default SpotifyPlaylist;
